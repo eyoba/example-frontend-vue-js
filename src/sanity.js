a0bbee1c95079af8683d13d7c76c1c4aee564e11
Eyob Teweldemedhin <eyob.teweldemedhin@nsd.no>
2021-03-06 09:57:47 +0100

    First commit

--- src/sanity.js
+++ src/sanity.js
@@ -5,7 +5,6 @@ export default sanityClient({
   projectId: "y17l1tvc",
   dataset: "production",
   useCdn: true
-  
   // useCdn == true gives fast, cheap responses using a globally distributed cache.
   // Set this to false if your application require the freshest possible
   // data always (potentially slightly slower and a bit more expensive).
