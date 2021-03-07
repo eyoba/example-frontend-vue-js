e84b2f897bb6274f2d181679f3379821ce6f894f
Eyob Teweldemedhin <eyob.teweldemedhin@nsd.no>
2021-03-07 23:00:30 +0100

    commit II

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
     //baseUrl: '/search/'
   productionSourceMap: false,
 
-  indexPath: 'public/index.html',
+ 
   baseUrl: process.env.NODE_ENV === 'production'
     ? '/search/'
     : '/'
