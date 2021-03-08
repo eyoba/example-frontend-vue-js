ccce499c250efeea8eb6e30d85eeb23e5cc094fd
Eyob Teweldemedhin <eyob.teweldemedhin@nsd.no>
2021-03-08 11:16:42 +0100

    commit II

--- vue.config.js
+++ vue.config.js
@@ -3,13 +3,13 @@
  */
 
 module.exports = {
-    //baseUrl: '/search/'
-  productionSourceMap: false,
+   baseUrl: '/search/'
+  //productionSourceMap: false,
   //outputDir: '../dist',
   //assetsDir: 'static',
-  baseUrl: process.env.NODE_ENV === 'production'
+ /* baseUrl: process.env.NODE_ENV === 'production'
     ? '/search/'
     : '/'
-  }
+  }*/
 
 
