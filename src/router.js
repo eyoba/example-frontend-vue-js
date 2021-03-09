b2031d0350e1bea6dc997677810b4079f488551f
Eyob Teweldemedhin <eyob.teweldemedhin@nsd.no>
2021-03-09 07:27:27 +0100

    commit II

--- src/router.js
+++ src/router.js
@@ -5,34 +5,34 @@ import Movies from "./views/Movies.vue";
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
+  //mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "eyob/",
+      path: "/",
       name: "index",
       component: Movies
     },
     {
-      path: "eyob/movies",
+      path: "/movies",
       name: "movies",
       component: Movies
     },
     {
-      path: "eyob/people",
+      path: "/people",
       name: "people",
       component: () =>
         import(/* webpackChunkName: "people" */ "./views/People.vue")
     },
     {
-      path: "eyob/movie/:id",
+      path: "/movie/:id",
       name: "movie",
       component: () =>
         import(/* webpackChunkName: "movie" */ "./views/Movie.vue"),
       props: route => ({ id: route.params.id })
     },
     {
-      path: "eyob/people/:id",
+      path: "/people/:id",
       name: "person",
       component: () =>
         import(/* webpackChunkName: "person" */ "./views/Person.vue"),
